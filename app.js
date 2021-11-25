6472d721ebd4e978ec41665e316cc6f30db5a2f5
Victoria Nemenova <vikalangor@gmail.com>
2021-11-26 01:50:34 +0300

    cors

--- app.js
+++ app.js
@@ -14,17 +14,7 @@ const { DEV_MONGO_URL } = require('./utils/dev-config');
 const { PORT = 3000, NODE_ENV, MONGO_URL } = process.env;
 const app = express();
 
-app.use(cors({
-  credentials: true,
-  origin: [
-    'https://frontend.nemenova.nomoredomains.monster',
-    'http://frontend.nemenova.nomoredomains.monster',
-    'https://localhost:3000',
-    'http://localhost:3000',
-    'https://localhost:3001',
-    'http://localhost:3001',
-  ],
-}));
+app.use(cors());
 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : DEV_MONGO_URL, {
   useNewUrlParser: true,
